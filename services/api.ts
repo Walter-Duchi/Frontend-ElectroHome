652f4b0762843a51e255f965f80f7f687e31ec64
Walter-Duchi <waltduchi@gmail.com>
2026-04-17 20:31:05 -0500

    nos conectamos a la api de production

--- services/api.ts
+++ services/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5298/api'; // Tu backend
+const API_URL = 'https://eccomerce.somee.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
